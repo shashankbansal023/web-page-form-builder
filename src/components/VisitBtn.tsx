52033b120396c071b047e07b57573ba12510a927
shashankbansal023 <shashankbansal023@gmail.com>
2024-03-19 12:17:17 +0530

    feat: add preview and publish btn

---
+++ src/components/VisitBtn.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import React, { useEffect, useState } from 'react'
+import { Button } from './ui/button';
+
+const VisitBtn = ({shareUrl} : {shareUrl : string}) => {
+    const [mounted , setMounted] = useState(false);
+    const shareLink = `${window.location.origin}/submit/${shareUrl}`
+
+    useEffect(() => {
+        setMounted(true);
+    }, [])
+
+    if(!mounted) return null;
+  return (
+    <Button className='w-[200px]' 
+    onClick={() => {
+        window.open(shareLink, '_blank');
+    }}>
+         Visit
+    </Button>
+  )
+}
+
+export default VisitBtn
